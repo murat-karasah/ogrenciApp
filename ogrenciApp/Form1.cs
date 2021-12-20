a68e7b3bbc88c9097ccae14a977d0d0178b56677
Murat Karasah <mkarasah@gmail.com>
2021-12-20 14:34:30 +0300

    Add project files.

---
+++ ogrenciApp/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ogrenciApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Ogrenci> olist = new List<Ogrenci>();
+        Random rnd = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ogrenci o1 = new Ogrenci();
+            
+            o1.ad = textBox1.Text;
+            o1.soyad = textBox2.Text;
+            o1.bolum = comboBox1.SelectedItem.ToString();
+            o1.sinif = Convert.ToInt32(comboBox2.SelectedItem);
+
+
+            
+            o1.no = rnd.Next(1000, 5000).ToString();
+            olist.Add(o1);
+
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<Ogrenci> o = new List<Ogrenci>();
+            foreach (var item in olist)
+            {
+                Ogrenci ogrnci = new Ogrenci();
+                ogrnci.no = item.no;
+                ogrnci.ad = item.ad;
+                ogrnci.soyad = item.soyad;
+                ogrnci.bolum = item.bolum;
+                ogrnci.sinif = item.sinif;
+
+                o.Add(ogrnci);
+            }
+
+            dataGridView1.DataSource = o;
+
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ogrenciApp/Ogrenci.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogrenciApp
+{
+   public class Ogrenci
+    {
+
+
+        public int s;
+        public Ogrenci()
+        {
+           
+
+        }
+        string d;
+        private string _no;
+        public string no { get {
+                int number;
+                bool success = int.TryParse(_no, out number);
+
+                if (success)
+                {
+                    _no = _bolum.Substring(0, 1).ToUpper() + _no;
+
+                }
+
+
+                return _no; } set { _no = value; } }
+
+        private string _ad;
+        public string ad { get {return _ad; } set { _ad=value; } }
+
+        private string _soyad;
+        public string soyad { get {return _soyad ; } set {_soyad = value; } }
+
+        private string _bolum;
+        public string bolum { get {return _bolum; } set {_bolum = value; } }
+
+        private int _sinif;
+        public int sinif{ get { return _sinif; } set {_sinif = value; } }
+
+
+    }
+}
